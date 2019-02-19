516aaafef7b2926446a398e1b62815ef9360ef9a
Max Levitskiy <max.dstu@gmail.com>
2019-02-19 14:39:37 +0100

    jenkins-3 fix artifactory publish

--- src/org/webtree/jenkins/pipeline/Artifactory.groovy
+++ src/org/webtree/jenkins/pipeline/Artifactory.groovy
@@ -1,13 +1,19 @@
 package org.webtree.jenkins.pipeline
+
 class Artifactory {
     Script script
-    def publish(String credentialsId, String artifactoryPrefix, String jarPath) {
-        def version = script.sh(
-                script: 'mvn -q -Dexec.executable=echo -Dexec.args=\'${project.version}\' --non-recursive exec:exec',
-                returnStdout: true
-        ).trim()
-        script.withCredentials([script.usernamePassword(credentialsId: credentialsId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            script.sh "curl -u ${script.USERNAME}:${script.PASSWORD} -X put https://artifactory.webtree.org/artifactory/list/dev/${artifactoryPrefix}/${version}/ -T ${jarPath}-${version}.jar"
-        }
+
+    def publish(String artifactoryId) {
+        script.rtMavenDeployer(
+                id: 'webtree-maven-deployer',
+                serverId: "${artifactoryId}",
+                releaseRepo: 'libs-release',
+                snapshotRepo: 'dev'
+        )
+
+        script.rtMavenRun(
+                goals: 'clean install',
+                deployerId: 'webtree-maven-deployer',
+        )
     }
 }
